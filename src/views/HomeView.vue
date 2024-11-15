eed83fb7159be24f07aba438718bcca1c4d9fc7c
Divs-Git <divyansh.sri258@gmail.com>
2024-11-15 22:51:37 +0530

    added initital home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
-<script setup></script>
-
 <template>
   <main>
-    <div>Home</div>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        {{ project.title }}
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  mounted() {
+    fetch('http://localhost:8080/projects')
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err))
+  },
+}
+</script>
