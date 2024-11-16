aaf2f952553aa1e2566f61eac9723e5bb2085335
Divs-Git <divyansh.sri258@gmail.com>
2024-11-16 10:42:51 +0530

    added form to add project

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/HomeView.vue'),
     },
+    {
+      path: '/add',
+      name: 'addProject',
+      component: () => import('../views/AddProject.vue'),
+    },
   ],
 })
 
---
+++ src/views/AddProject.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label for="title">Title</label>
+    <input type="text" name="title" id="title" v-model="title" />
+    <label for="details">Details</label>
+    <textarea name="details" id="details" v-model="projectDetails"></textarea>
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddProject',
+  data() {
+    return {
+      title: '',
+      projectDetails: '',
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.title, this.projectDetails)
+    },
+  },
+}
+</script>
+
+<style scoped>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
+</style>
